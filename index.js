02417d4c9defb4db67b1ca6bd80e7dc5578a1f87
adamctr <adam.courtaro@gmail.com>
2022-03-10 22:13:46 +0100

    add var js

--- index.js
+++ index.js
@@ -6,5 +6,25 @@ const temps = document.getElementById('temps');
 const precipitation = document.getElementById('precipitation');
 const humidity = document.getElementById('humidity');
 const wind = document.getElementById('wind');
-const nxtdays = document.getElementsByClassName('nxtdays');
+const cards = document.getElementsByClassName('cards');
 const iconnxtdays = document.getElementsByClassName('iconnxtdays');
+
+// Texte milieu CARTES
+cards[0].children[1].innerHTML = 'hey';
+cards[1].children[1].innerHTML = 'hey';
+cards[2].children[1].innerHTML = 'hey';
+cards[3].children[1].innerHTML = 'hey';
+
+//Texte haut CARTES
+
+// cards[0].children[0].innerHTML = 'hey bg';
+// cards[1].children[0].innerHTML = 'hey bg';
+// cards[2].children[0].innerHTML = 'hey bg';
+// cards[3].children[0].innerHTML = 'hey bg';
+
+// Texte bas CARTES
+
+cards[0].children[2].innerHTML = 'cc';
+cards[1].children[2].innerHTML = 'cc';
+cards[2].children[2].innerHTML = 'cc';
+cards[3].children[2].innerHTML = 'cc';
