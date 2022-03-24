6861c6d94d2e257631819c619b11c246693c0047
Adam <adam.courtaro@gmail.com>
2022-03-24 18:46:35 +0100

    changes

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-const day = document.getElementById('day');
-const date = document.getElementById('date');
-const iconactualweather = document.getElementById('iconactualweather');
-const temperature = document.getElementById('temperature');
-const temps = document.getElementById('temps');
-const precipitation = document.getElementById('precipitation');
-const humidity = document.getElementById('humidity');
-const wind = document.getElementById('wind');
-const cards = document.getElementsByClassName('cards');
-const iconnxtdays = document.getElementsByClassName('iconnxtdays');
+const day = document.getElementById("day");
+const date = document.getElementById("date");
+const iconactualweather = document.getElementById("iconactualweather");
+const temperature = document.getElementById("temperature");
+const temps = document.getElementById("temps");
+const precipitation = document.getElementById("precipitation");
+const humidity = document.getElementById("humidity");
+const wind = document.getElementById("wind");
+const cards = document.getElementsByClassName("cards");
+const iconnxtdays = document.getElementsByClassName("iconnxtdays");
 
 // Texte milieu CARTES
-cards[0].children[1].innerHTML = 'hey';
-cards[1].children[1].innerHTML = 'hey';
-cards[2].children[1].innerHTML = 'hey';
-cards[3].children[1].innerHTML = 'hey';
+cards[0].children[1].textContent = "hey";
+cards[1].children[1].textContent = "hey";
+cards[2].children[1].textContent = "hey";
+cards[3].children[1].textContent = "hey";
 
 //Texte haut CARTES
 
@@ -24,7 +24,9 @@ cards[3].children[1].innerHTML = 'hey';
 
 // Texte bas CARTES
 
-cards[0].children[2].innerHTML = 'cc';
-cards[1].children[2].innerHTML = 'cc';
-cards[2].children[2].innerHTML = 'cc';
-cards[3].children[2].innerHTML = 'cc';
+cards[0].children[2].textContent = "cc";
+cards[1].children[2].textContent = "cc";
+cards[2].children[2].textContent = "cc";
+cards[3].children[2].textContent = "cc";
+/////////////
+const day1 = document.getElementById("day1");
